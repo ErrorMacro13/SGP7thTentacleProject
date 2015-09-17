224b543b6b88a0e6adac8888b560f57e7efb7f67
Ben <br_owens1116@comcast.net>
2015-09-17 16:14:24 -0400

    Pendulum

--- Assets/Scripts/Obstacles/Lethal/PendulumBehavior.cs
+++ Assets/Scripts/Obstacles/Lethal/PendulumBehavior.cs
@@ -3,13 +3,32 @@ using System.Collections;
 
 public class PendulumBehavior : MonoBehaviour {
 
+    public HingeJoint2D hinge;
+    JointMotor2D motor;
+    bool swingingLeft;
 	// Use this for initialization
 	void Start () {
-	
+        hinge = gameObject.GetComponent<HingeJoint2D>();
+        motor = hinge.motor;
+        motor.maxMotorTorque = 100000;
+        motor.motorSpeed = 100;
+        swingingLeft = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (hinge.jointAngle >= 179.0f && swingingLeft)
+        {
+            swingingLeft = false;
+            motor.motorSpeed = -motor.motorSpeed;
+            hinge.motor = motor;
+        }
+
+        if (hinge.jointAngle <= 1.0f && !swingingLeft)
+        {
+            swingingLeft = true;
+            motor.motorSpeed = -motor.motorSpeed;
+            hinge.motor = motor;
+        }
 	}
 }
