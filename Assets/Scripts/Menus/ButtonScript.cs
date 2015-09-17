006b07440870144413f66ca20daa93f62524d966
Cody Felts <codyfelts@yahoo.com>
2015-09-17 13:51:25 -0400

    Menus using arrow keys and WASD, also sounds working except for when mouse hovers over

--- Assets/Scripts/Menus/ButtonScript.cs
+++ Assets/Scripts/Menus/ButtonScript.cs
@@ -1,25 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ButtonScript : MonoBehaviour {
+public class ButtonScript : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public GameObject SoundManager;
+
+    // Use this for initialization
+    void Start()
+    {
+        SoundManager = GameObject.Find("SoundManager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     public void ButtonClick(string name)
     {
+        if (name != "Back")
+            SoundManager.SendMessage("In");
+        else
+            SoundManager.SendMessage("Out");
         Application.LoadLevel(name);
     }
 
     public void ChangeImages(string name)
     {
         SendMessageUpwards(name);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    void OnMouseEnter()
+    {
+        SoundManager.SendMessage("Hovered");
     }
 }
