1c78bb4fb52e8752a24fb724227c1dba3bf93657
Cody Felts <codyfelts@yahoo.com>
2015-09-24 13:20:59 -0400

    Lasers, SlipperyFloors

--- Assets/Scripts/Obstacles/Lethal/LaserTurretBehavior.cs
+++ Assets/Scripts/Obstacles/Lethal/LaserTurretBehavior.cs
@@ -1,15 +1,72 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LaserTurretBehavior : MonoBehaviour {
+public class LaserTurretBehavior : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    float CurrGameSpeed = 1.0f;
+    public float InitialDelay = 1.0f;
+    public float ChargeUpTime = 1.0f;
+    public float FireTime = 1.0f;
+    public float BeamWidth = 1.0f;
+    public bool On = false;
+
+    public GameObject Beam;
+    public GameObject LaserCatcher;
+
+    // Use this for initialization
+    void Start()
+    {
+        Beam.transform.localPosition = LaserCatcher.transform.localPosition / 2;
+        if (On)
+            Beam.transform.localScale = new Vector3(Beam.transform.localPosition.x * 2 - 1, BeamWidth, 1);
+        else
+            Beam.transform.localScale = new Vector3(0,0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (InitialDelay <= 0.0f)
+        {
+            if (On)
+            {
+                On = false;
+                InitialDelay = ChargeUpTime;
+                Beam.transform.localScale = new Vector3(0, 0, 0);
+            }
+            else
+            {
+                On = true;
+                InitialDelay = FireTime;
+                Beam.transform.localScale = new Vector3(Beam.transform.localPosition.x * 2 - 1, BeamWidth, 1);
+            }
+        }
+        else
+            InitialDelay -= (Time.deltaTime * CurrGameSpeed);
+    }
+
+    void SetTime(short GameSpeed)
+    {
+        switch (GameSpeed)
+        {
+            case 1:
+                CurrGameSpeed = 0.5f;
+                break;
+            case 2:
+                CurrGameSpeed = 0.25f;
+                break;
+            case 3:
+                CurrGameSpeed = 0.0f;
+                break;
+            default:
+                CurrGameSpeed = 1.0f;
+                break;
+        }
+    }
 }
